ab441c2f1d1f1599bc0e724f54f2266a45a0ba43
Kosta <Kosta@DESKTOP-P1A4LEE>
2024-03-13 15:04:03 +0900

    fdsfsdf

---
+++ stock/src/Trade/Trade.java
@@ -0,0 +1,66 @@
+package Trade;
+
+import java.sql.Date;
+
+public class Trade {
+	private int tradeId;
+	private int stockId;
+	private int sellt;
+	private int buy;
+	private int traderNum;
+	private int price;
+	private Date tradeDate;
+	public Trade(int tradeId, int stockId, int sellt, int buy, int traderNum, int price, Date tradeDate) {
+		super();
+		this.tradeId = tradeId;
+		this.stockId = stockId;
+		this.sellt = sellt;
+		this.buy = buy;
+		this.traderNum = traderNum;
+		this.price = price;
+		this.tradeDate = tradeDate;
+	}
+	public int getTradeId() {
+		return tradeId;
+	}
+	public void setTradeId(int tradeId) {
+		this.tradeId = tradeId;
+	}
+	public int getStockId() {
+		return stockId;
+	}
+	public void setStockId(int stockId) {
+		this.stockId = stockId;
+	}
+	public int getSellt() {
+		return sellt;
+	}
+	public void setSellt(int sellt) {
+		this.sellt = sellt;
+	}
+	public int getBuy() {
+		return buy;
+	}
+	public void setBuy(int buy) {
+		this.buy = buy;
+	}
+	public int getTraderNum() {
+		return traderNum;
+	}
+	public void setTraderNum(int traderNum) {
+		this.traderNum = traderNum;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Date getTradeDate() {
+		return tradeDate;
+	}
+	public void setTradeDate(Date tradeDate) {
+		this.tradeDate = tradeDate;
+	}
+	
+}
